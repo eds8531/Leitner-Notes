acc50273cc65addb19045e224d2a9eb104432b44
eds8531 <44855388+eds8531@users.noreply.github.com>
2019-05-12 21:01:40 -0400

    Update flashcards1.py

--- flashcards1.py
+++ flashcards1.py
@@ -2,7 +2,7 @@ import shelve
 import datetime
 from datetime import timedelta, datetime
 
-# Creates or alters a shelf file fcdata. I think it sets the output file to '[]'
+#  is a function for developers only and will not be in the finished program. It resets the shelf file where words are saved to a blank list.
 
 def init():
     sure = input("Are you sure you want to recreate the shelf file (y/n)?  ")
@@ -15,12 +15,14 @@ def init():
     else:
         main()
 
+# Another developer function that I use to run various tasks. Not part of the final program.
 def six():
     shelfFile = shelve.open('fcdata')
     output = shelfFile['output']
     print(output)
     for i in output:
         print(i)
+        
 # A method that adds words and definitions from 'Notes1.txt' to the shelfFile output list
 
 def update():
@@ -55,15 +57,13 @@ def update():
             output.append(card)
 
     shelfFile['output'] = output
-    print(len(output))
-    for x in output:
-        print(x)
     shelfFile.close()
     print('\n\nUpdate Sucessful.\n\n')
     print(str(l) + ' new cards added.')
     main()
 
-# Prints a list of today's cards
+# Prints a list of today's words and definitions. 
+
 def print_list():
     shelfFile = shelve.open('fcdata')
     print("\n\n\nLIST OF TODAY'S WORDS\n\n\n")
@@ -73,6 +73,7 @@ def print_list():
     shelfFile.close()
     main()
 
+# Quizes user with that day's flashcards.
 def fc():
     word_list_today = []
     shelfFile = shelve.open('fcdata')
@@ -126,6 +127,8 @@ def fc():
         main()
     else:
         pass
+
+# A menu function. Option 4 has not been added yet.
 
 def main():
     print("\n\n\nFlashcards: Main Menu\n\n")
